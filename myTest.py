c523ca28b8a3d887cf48f3088a4ed15a4af15c2b
Aminzai <lagunawang@gmail.com>
2011-08-01 13:18:03 +0800

    add unittest py

---
+++ myTest.py
@@ -0,0 +1,36 @@
+import unittest
+
+from sdt.SystemInfo import SystemInfo
+
+class TestSystemInfo(unittest.TestCase):
+    def setUp(self):
+        self.d = SystemInfo()
+
+    def test_getStr(self):
+        """Print __str__()"""
+        pass
+        #print(self.d.__str__())
+
+    def test_getOsType(self):
+        """get os type"""
+        print "OS Type:", self.d.getOsType()
+
+    def test_getDistName(self):
+        """ get dist name """
+        print "getDistName:", self.d.getDistName()
+
+    def test_getArch(self):
+        """ getArch """
+        print "getArch:", self.d.getArch()
+
+    def test_getVersion(self):
+        """ getVersion """
+        print "getVersion:", self.d.getVersion()
+
+    def test_getLocale(self):
+        """ getLocale"""
+        print 'd.getLocale()',self.d.getLocale()
+
+
+if __name__ == "__main__":
+    unittest.main()
